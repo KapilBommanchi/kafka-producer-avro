411b5309d79a323654f39cc93ca0d85ff63b0215
Kapil Bommanchi <b.kapilcool@gmail.com>
2022-07-31 17:25:00 +0530

    Initial Commit for Springboot Kafka Producer AVRO

---
+++ src/main/java/com/kapil/kafka/example/kafkaproduceravro/controller/SpringAvroRestController.java
@@ -0,0 +1,30 @@
+package com.kapil.kafka.example.kafkaproduceravro.controller;
+
+import com.kapil.kafka.example.kafkaproduceravro.model.StockHistoryModel;
+import com.kapil.kafka.example.kafkaproduceravro.schema.StockHistory;
+import com.kapil.kafka.example.kafkaproduceravro.service.SpringAvroProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class SpringAvroRestController {
+    @Autowired
+    private SpringAvroProducer springAvroProducer;
+
+    @PostMapping(value = "/sendStockHistory")
+    public void sendStockHistory(@RequestBody StockHistoryModel model) {
+        StockHistory stockHistory = StockHistory.newBuilder().build();
+        stockHistory.setStockName(model.getStockName());
+        stockHistory.setTradeType(model.getTradeType());
+        stockHistory.setPrice(model.getPrice());
+        stockHistory.setAmount(model.getAmount());
+        stockHistory.setTradeId(new Random(1000).nextInt());
+        stockHistory.setTradeMarket(model.getTradeMarket());
+        stockHistory.setTradeQuantity(model.getTradeQuantity());
+        springAvroProducer.send(stockHistory);
+    }
+}
---
+++ src/test/java/com/kapil/kafka/example/kafkaproduceravro/KafkaProducerAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapil.kafka.example.kafkaproduceravro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
